e985468ab78aae12fd1600efda5b77e5b023b341
Mack Grissom <mcintyregrissom@gmail.com>
2022-11-05 16:17:46 +0700

    added temporary assets folder for initial seeding

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <div className='hero-banner-container'>
+      <p className='beats-solo'> SMALL TEXT</p>
+<h3> MID TEXT</h3>
+
+<img src='' alt='tshirt' className='hero-banner-image' />
+
+<div> 
+  <Link href='/product/ID'> 
+  <button type='button'> Button Text </button>
+  </Link>
+  <div className='desc'>
+    <h5> Description...</h5> 
+    <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate voluptatem ea mollitia aspernatur assumenda est similique quidem maxime qui magnam. Repellat aut commodi ab corporis atque autem enim a quisquam!</p>
+
+  </div>
+</div>
+
+
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Product, FooterBanner, HeroBanner} from '../components'
+import {client} from '../lib/client';
+import banner from '../sanity_raknarin/schemas/banner';
 
 const index = () => {
   return (
@@ -20,5 +22,16 @@ const index = () => {
     </>
     )
   }
+  export const getServerSideProps =  async () => {
+    const query = '*[_type == "product"]';
+    const products = await client.fetch(query);
+
+    const bannerQuery = '*[type == "banner"]';
+    const bannerData = await client.fetch(bannerQuery);
+
+    return {
+      props: {products, bannerData}
+    }
+  }
+  export default home
   
-  export default index
