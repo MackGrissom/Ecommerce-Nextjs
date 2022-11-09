5cc7568e86151d34d0ea2dab0f2c0d9d1a83d182
Mack Grissom <mcintyregrissom@gmail.com>
2022-11-10 06:49:38 +0700

    Removed description on banner

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = ({ products, bannerData }) => (
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
     <div className="products-heading">
       <h2>Best Seller Products</h2>
-      <p>speaker There are many variations passages</p>
+      <p>These are our best selling designs - limited supply</p>
     </div>
 
     <div className="products-container">
