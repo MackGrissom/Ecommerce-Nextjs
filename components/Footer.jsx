1a6d338b9e84c3ce2c08dd6eb5c6b6a76492d6fa
Mack Grissom <mcintyregrissom@gmail.com>
2022-11-10 13:57:16 +0700

    added footer, navbar and layout

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
+import Link from 'next/link';
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
-
-export default Footer
+    <div className="footer-container">
+    <p>2022 Raknarin Clothing LLC All rights reserverd</p>
+    <p className="icons">
+    <a href='www.instagram.com/saintofthebeachofficial'>
+    <AiFillInstagram />
+    </a>
+    </p>
+    </div>
+    )
+  }
+  
+  export default Footer
--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import Link from 'next/link';
 import { urlFor } from '../lib/client';
 
+
 const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
+      
       <div>
         <p className="beats-solo">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
+        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image lg:visible" />
 
         <div>
           <Link href={`/product/${heroBanner.product}`}>
