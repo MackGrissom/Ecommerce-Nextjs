ed5b59d1117915866973fe084488d9aa37007082
Mack Grissom <mcintyregrissom@gmail.com>
2022-11-11 13:02:15 +0700

    added state context - but slug page and updated app js to add state context wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,15 @@
 import React from 'react'
 import '../styles/globals.css'
 import { Layout } from '../components'
+import {StateContext} from '../context/StateContext'
 
 function MyApp({ Component, pageProps }) {
   return( 
+    <StateContext>
     <Layout>
   <Component {...pageProps} />
   </Layout>
+  </StateContext>
   )
 }
 
